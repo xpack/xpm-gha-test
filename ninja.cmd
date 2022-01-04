692513d8735932c825585ae2eb505ced2e88fb04
Liviu Ionescu <ilg@livius.net>
2022-01-04 22:37:03 +0200

    ci try our cmd

---
+++ ninja.cmd
@@ -0,0 +1,2 @@
+@SET Path=C:\Users\runneradmin\AppData\Roaming\xPacks\@xpack-dev-tools\ninja-build\1.10.2-5.1\.content\bin;%Path%
+@"C:\Users\runneradmin\AppData\Roaming\xPacks\@xpack-dev-tools\ninja-build\1.10.2-5.1\.content\bin\ninja.exe"   %*
